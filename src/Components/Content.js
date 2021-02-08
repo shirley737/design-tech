cdc9cdacfd701ea2f1430aaa00a1e79662d62f18
Shirley <yug068@ucsd.edu>
2021-02-07 22:31:42 -0800

    update visuals

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -4,8 +4,20 @@ import "./Content.scss";
 function Content() {
   return (
     <div className="join">
-      <div className="community">Join the community!</div>
+      <div className="context">
+        <div className="community">
+          Join the community!
+        </div>
+        <div className="contact">
+          <div className="email"><a href="mailto: design.technologists@gmail.com">Email Us</a> </div>
+          <div className="slack"> <a href="">Join our Slack Workplace</a> </div>
+        </div>
+
+
+      </div>
+
     </div>
+
   );
 }
 
