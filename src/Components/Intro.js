034a41b3d2bcf41b0618c0ed2d4f98489ac1d071
Shirley <yug068@ucsd.edu>
2021-02-08 10:01:01 -0800

    update content

--- src/Components/Intro.js
+++ src/Components/Intro.js
@@ -6,7 +6,8 @@ function Intro() {
     <div className="intro">
       <div className="textbox">
         <div className="title"> What is Design.Tech? </div>
-        <div className="answer"> Primary based on the Bay area, we're a new community full of technologists of design. More details will come soon! </div>
+        <div className="answer">  </div><p>Living at the intersection of front-end technology and human-centric design, design technologists are powerful intermediaries between designers and developers.</p>
+        <div className="answer">  </div><p>Founded in 2021, The Design Technology Club is a new community full of amazing design technologists. Through networking and social events, our goal is to connect design technologists across various companies in California. For contact information, please see below. </p>
       </div>
     </div>
   );
