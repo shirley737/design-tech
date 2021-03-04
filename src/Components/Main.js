d7481d1f768f01e6ac2310c20ad87941e8d3e5d1
Shirley Guo <shirley_guo@intuit.com>
2021-03-04 10:51:17 -0800

    Update Main.js

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -6,14 +6,13 @@ function Main() {
   return (
     <div className="container">
       <div className="content">
-        <div className="header">Design</div>;
-        <div className="header">Technology</div>;
-        <div className="header">Club</div>;
+        <div className="header">Design</div>
+        <div className="header">Technology</div>
+        <div className="header">Club</div>
         <div className="sub-header">
           A place for design technologists to connect and thrive.
         </div>
       </div>
-      ;
     </div>
   );
 }
