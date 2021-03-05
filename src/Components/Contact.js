98fa7db5bd7bd247a1eeedab8ab2a1dfeab57447
Shirley Guo <yug068@ucsd.edu>
2021-03-04 20:42:29 -0800

    center the text

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -4,21 +4,38 @@ import "./Contact.scss";
 function Contact() {
   return (
     <div className="join">
-      <div className="context">
-        <div className="community">
-          Join the community!
+      <div className="join-context">
+        <div className="text">
+          <div className="community">Join the community!</div>
+          <div className="contact">
+            <div>
+              <span className="email"> Email :</span>
+              <span>
+                <a href="mailto: design.technologists@gmail.com">
+                  {" "}
+                  design.technologists@gmail.com
+                </a>
+              </span>
+            </div>
+            <div>
+              <span className="slack"> Slack :</span>{" "}
+              <span>
+                <a
+                  href="https://docs.google.com/forms/d/e/1FAIpQLSdoBp0UAVcarRlqP_BuBG_gJotcEPvUbckidYeRZh22wHZbNw/viewform?usp=sf_link"
+                  target="_blank"
+                >
+                  Submit an interest form
+                </a>
+              </span>{" "}
+            </div>
+          </div>
         </div>
-        <div className="contact">
-          <div><span className ="email"> Email :</span><span><a href="mailto: design.technologists@gmail.com"> design.technologists@gmail.com</a></span></div>
-          <div><span className ="slack"> Slack :</span> <span><a href="https://docs.google.com/forms/d/e/1FAIpQLSdoBp0UAVcarRlqP_BuBG_gJotcEPvUbckidYeRZh22wHZbNw/viewform?usp=sf_link" target="_blank">Submit an interest form</a></span> </div>
+        <div className="footer">
+          {" "}
+          Made with ❤ by the design technology club organizers.
         </div>
-
-       
-
       </div>
-      <div className= "footer"> Made with ❤ by the design technology club organizers.</div>
     </div>
-
   );
 }
 
