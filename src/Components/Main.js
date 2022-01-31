16e3a615582612fcdf5aae484338e330ed3014fc
Shirley <yug068@ucsd.edu>
2022-01-30 19:05:54 -0800

    update the frontpage

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,17 +1,48 @@
 import React from "react";
 import "./Main.scss";
 import image from "../images/image.jpg";
+import Typewriter from "typewriter-effect";
 
 function Main() {
   return (
     <div className="container">
       <div className="content">
-        <div className="header">Design</div>
-        <div className="header">Technology</div>
-        <div className="header">Club</div>
-        <div className="sub-header">
-          A place for design technologists to connect and thrive.
+        {" "}
+        <div>
+          <div className="header">We Are</div>
+          <br />
+          <div className="typewritter">
+            <Typewriter
+              onInit={(typewriter) => {
+                typewriter
+                  .typeString("Designers")
+                  .callFunction(() => {
+                    console.log("designer");
+                  })
+                  .pauseFor(2000)
+                  .deleteAll()
+                  .callFunction(() => {
+                    console.log("All strings were deleted");
+                  })
+                  .typeString("Developers")
+                  .pauseFor(2000)
+                  .deleteAll()
+                  .typeString("Researchers")
+                  .pauseFor(2000)
+                  .deleteAll()
+                  .typeString("Inventors")
+                  .pauseFor(2000)
+                  .deleteAll()
+                  .start();
+              }}
+              options={{
+                loop: true,
+              }}
+            />
+          </div>
         </div>
+        <br />
+        <div className="header">Who Bring Creative Ideas to Life.</div>
       </div>
     </div>
   );
